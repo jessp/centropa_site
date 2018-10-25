884a325c25f613b0b9a884a5d099d43bce6dee0d
Jess <jessicapeter92@gmail.com>
2018-10-25 12:19:01 +0200

    talks to wordpress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,44 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql } from "gatsby"
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+
+  render() {
+  	const data = this.props.data;
+  	return(
+	  <Layout>
+	    <div>
+	      <h1>My WordPress Blog</h1>
+	      <h4>Posts</h4>
+	      {data.allWordpressPost.edges.map(({ node }) => (
+	        <div key={node.id}>
+	          <p>{node.title}</p>
+	          <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+	        </div>
+	      ))}
+	    </div>
+	  </Layout>
+	)
+  }
+
+}
 
 export default IndexPage
+
+
+export const pageQuery = graphql`
+  query {
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+        	id
+          	title
+          	excerpt
+          	slug
+        }
+      }
+    }
+  }
+`
