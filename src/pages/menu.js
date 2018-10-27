b308211f9267a06a54a687eb8230468899e6b39b
Jess <jessicapeter92@gmail.com>
2018-10-27 22:13:59 +0200

    updated menu page

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,42 +1,56 @@
 import React from 'react'
-import { Link, graphql } from "gatsby"
-
-import '../css/index.css'
+import { graphql } from "gatsby"
+import {capitalizeFirstLetter} from "../utils/helpers"
 
 
 class Menu extends React.Component {
 
-	constructor(props){
-		super(props);
+  constructor(props){
+    super(props);
 
-		this.render_cat = this.render_cat.bind(this);
+    this.render_cat = this.render_cat.bind(this);
 
-	}
+  }
 
-	render_cat(cat_name, menu_items){
-		let items = menu_items.filter(function(d){
-	  		let item_exists = d.node.categories.filter(function(e){
-	  			return e.name === cat_name;
-	  		})
-	  		return item_exists.length > 0;
-	  	});
-		return (
-			<div>
-				<h1>{cat_name}</h1>
-			</div>
-		)
-	}
+  render_cat(cat_name, menu_items){
+    let items = menu_items.filter(function(d){
+        let item_exists = d.node.categories.filter(function(e){
+          return e.name === cat_name;
+        })
+        return item_exists.length > 0;
+      });
 
-	render() {
-	  	let menu_items = this.props.data.allWordpressPost.edges;
+    return (
+      <div>
+        <h2>{capitalizeFirstLetter(cat_name)}</h2>
+        <div>
+          {
+            items.map(function(item, index){
+              return (
+                <div key = {index}>
+                  <h3>{item.node.title + " " + item.node.acf.price}</h3>
+                  <p>{item.node.acf.description}</p>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    )
 
-	  	return (
-	  		<div>
-	  			<h1>{"Menu"}</h1>
-	  			{this.render_cat("sandwiches", menu_items)}
-	  		</div>
-	  	)
-	}
+  }
+
+  render() {
+      let menu_items = this.props.data.allWordpressPost.edges;
+
+      return (
+        <div>
+          <h1>{"Menu"}</h1>
+          {this.render_cat("drinks", menu_items)}
+          {this.render_cat("sandwiches", menu_items)}
+        </div>
+      )
+  }
 
 }
 
@@ -58,7 +72,7 @@ export const menuQuery = graphql`
                 name
               }
               tags {
-              	name
+                name
               }
               title
               acf{
