1acec3794c1fcfead82570341d473e15c35f69ed
Jess <jessicapeter92@gmail.com>
2018-11-26 14:19:42 +0100

    restyling story page

--- src/templates/book.js
+++ src/templates/book.js
@@ -3,90 +3,43 @@ import { graphql } from "gatsby"
 import PropTypes from "prop-types"
 import Layout from '../layouts/Layout'
 import Header from '../components/header';
-import PageTurner from '../components/PageTurner'
 import '../css/BookLayout.css'
+import GreenScrollDown from "./../images/green_scroll_down.svg";
+
 
 class PostTemplate extends Component {
 
   constructor(props){
     super(props);
-
-    this.scrollBehaviour = this.scrollBehaviour.bind(this);
-
-    this.state = {
-      pageNum: 0,
-      totalPages: 0
-    }
-
-    this.columnGap = 45;
   }
 
-  componentDidMount(){
-    let totalPages = Math.ceil(this.refs.spreadHolder.scrollWidth/(this.refs.spreadHolder.getBoundingClientRect().width + this.columnGap)) * 2;
-    this.setState({"totalPages": totalPages});
-  }
-
-  componentDidUpdate(prevProps, prevState){
-    //take into account column gap
-    let theLeft = (this.refs.spreadHolder.getBoundingClientRect().width + this.columnGap) * this.state.pageNum;
-    this.refs.spreadHolder.scrollLeft = theLeft;
-    let totalPages = Math.ceil(this.refs.spreadHolder.scrollWidth/(this.refs.spreadHolder.getBoundingClientRect().width + this.columnGap)) * 2;
-    
-    if (totalPages !== prevState.totalPages){
-          this.setState({"totalPages": totalPages});
-    }
-
-  }
-
-
-  scrollBehaviour(isForward){
-    if (isForward){
-      if (((this.state.pageNum + 1) * 2) < this.state.totalPages) {
-        this.setState({pageNum: (this.state.pageNum + 1)})
-      }
-    } else {
-      if (this.state.pageNum > 0) {
-        this.setState({pageNum: (this.state.pageNum - 1)})
-      }
-    }
-  }
 
   render() {
     const post = this.props.data.wordpressPost
     return (
         <div style={{"width": "100%", "height": "100%", "position": "relative"}}>
-          <Header minimal={true}/>
-          <div style={{"width": "100%", "height": "calc(100% - 40px)", "position": "relative", "paddingTop": "20px"}}>
-            <PageTurner left={true} doClick={() => this.scrollBehaviour(false)} disabled={(this.state.pageNum <= 0)}/>
-            <div style={{"width":"calc(100% - 80px)", "height": "100%", "float": "left", "overflow": "hidden"}}>
-              <div className={"pageHolder"} 
-                    style={{"float": "left"}}>
-                <h6 dangerouslySetInnerHTML={{ __html: post.title }} className={"centerText"} />
-              </div>
-              <div className={"pageHolder"} 
-                    style={{"float": "right"}}>
-                <h6 dangerouslySetInnerHTML={{ __html: post.acf.author_name }} className={"centerText"} />
-              </div>
-              <div className={"clearer"}/>
-              <div className={"spreadHolder"} 
-                   style={{"columnGap": this.columnGap}}
-                   ref={"spreadHolder"} 
-                   >
-                <div  dangerouslySetInnerHTML={{ __html: post.content }} 
-                      style={{"height":"100%", "width":"100%", "position": "relative"}}
-                       />
-              </div>
-              <div className={"pageHolder"} 
-                    style={{"float": "left"}}>
-                <h6 dangerouslySetInnerHTML={{ __html: (this.state.pageNum * 2 + 1) }} />
-              </div>
-              <div className={"pageHolder"} 
-                    style={{"float": "right"}}>
-                <h6 dangerouslySetInnerHTML={{ __html: (this.state.pageNum * 2 + 2) }} className={"rightText"} />
+          <Header/>
+          <div className={"storyHeaderHolder"}>
+            <div className={"storyLocationImageHolder"} style={{"backgroundImage": "url(" + post.acf.location_photo.source_url + ")"}}/>
+            <div className={"storyAuthorImageHolder"} style={{"backgroundImage": "url(" + post.acf.author_photo.source_url + ")"}}/>
+            <div className={"storyNameHolder"}>
+              <div className={"storyMetaText"}>
+                <h1>{post.title}</h1>
+                <h2>{"Inspired by " + post.acf.country_name}</h2>
+                <p>{"“It will not happen. She’ll continue the game, the same approach, the indifference when she meets him in public, in the café they both like to go to after work.”"}</p>
               </div>
             </div>
-            <PageTurner doClick={() => this.scrollBehaviour(true)} disabled={((this.state.pageNum * 2 + 2) >= this.state.totalPages)}/>
+            <div className={"authorBio"}>
+              <h1>{post.acf.author_name}</h1>
+              <p>
+                {"This author is great. This is his bio. He wrote some great books. Really solid stuff. Won awards, so good."}
+              </p>
+            </div>
           </div>
+          <div className={"greenScrollIndicator"}>
+              <img src={GreenScrollDown}/>
+          </div>
+          <div className={"storyHolder"} dangerouslySetInnerHTML={{ __html: post.content }}/>
         </div>
     )
   }
@@ -106,6 +59,13 @@ export const pageQuery = graphql`
       content
       acf {
         author_name
+        country_name
+        author_photo {
+          source_url
+        }
+        location_photo {
+          source_url
+        }
       }
     }
     site {
