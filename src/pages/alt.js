fed1f8bf8b4200cdb1bea16ee1969547835a0ca9
Jess <jessicapeter92@gmail.com>
2018-12-02 14:11:19 +0100

    work on styling index; about to pivot to more unstructured design

--- src/pages/alt.js
+++ src/pages/alt.js
@@ -13,7 +13,8 @@ class IndexPage extends React.Component {
 
     this.state ={
       "animationCompleted": false,
-      "colours": shuffle(["#da852d", "#a4384d", "#2a8fa3", "#efc835", "#fff", "#da852d", "#a4384d", "#2a8fa3", "#efc835", "#fff"])
+      "colours": shuffle(["#da852d", "#a4384d", "#2a8fa3", "#efc835", "#fff", "#da852d", "#a4384d", "#2a8fa3", "#efc835", "#fff"]),
+      "contributorIndex": 0
     }
 
     this.completeAnimation = this.completeAnimation.bind(this);
@@ -25,34 +26,38 @@ class IndexPage extends React.Component {
     this.setState({animationCompleted: true});
   }
 
-  rendergrid(){
+  rendergrid(authorPackage){
     let colours = this.state.colours;
     return (
       <div className={"grid1 monGrid"}>
         {Array.apply(null, Array(10)).map(function (_, i) {return i;}).map(function(d, i){
           let whichBg = "none";
+          let colourBg = colours[d];
           if (d === 2){
-            whichBg = "url(http://placekitten.com/600/600)";
+            whichBg = "url(" + authorPackage.location_photo + ")";
+            colourBg = "none"
           } else if (d === 3){
-            whichBg = "url(http://placekitten.com/400/400)";
+            whichBg = "url(" + authorPackage.food_photo + ")";
+            colourBg = "none"
           } else if (d === 7){
-            whichBg = "url(http://placekitten.com/500/500)";
+            whichBg = "url(" + authorPackage.author_photo + ")";
+            colourBg = "none"
           }
           let textColour = "#000";
           if (colours[d] === "#da852d" || colours[d] === "#a4384d" || colours[d] === "#2a8fa3"){
             textColour = "#fff";
           }
           return (
-            <div key = {i} style={{"backgroundColor": colours[d], "gridArea": ("grid" + d), 
+            <div key = {i} style={{"backgroundColor": colourBg, "gridArea": ("grid" + d), 
                 "backgroundImage": whichBg}}>
               {
                 d === 6 && 
                 <div className={"gridText"} style={{"color": textColour}}>
-                  <p>{"Ham on Rye"}</p>
+                  <p>{authorPackage.sandwich}</p>
                   <p>{"inspired by"}</p>
-                  <p>{"Ola Nordman"}</p>
+                  <p>{authorPackage.author}</p>
                   <p>{"inspired by"}</p>
-                  <p>{"Paris"}</p>
+                  <p>{authorPackage.country}</p>
                 </div>
               }
             </div>
@@ -64,13 +69,25 @@ class IndexPage extends React.Component {
 
 
   render() {
+    let slideNode = this.props.data.allContributors.edges[this.state.contributorIndex].node;
+    let slideObject = {};
+    slideObject["author"] = slideNode.acf.author_name;
+    slideObject["sandwich"] = slideNode.acf.sandwich_name;
+    slideObject["country"] = slideNode.acf.country_name;
+    slideObject["author_photo"] = slideNode.acf.author_photo.source_url;
+    slideObject["location_photo"] = slideNode.acf.location_photo.source_url;
+    let sandwichPhoto = this.props.data.food.edges.find(function(sandwich){
+      return sandwich.node.title === slideObject["sandwich"];
+    }).node.acf;
+    slideObject["food_photo"] = sandwichPhoto.food_photo ? sandwichPhoto.food_photo.source_url : "http://placekitten.com/600/600";
+    
   	return(
   	  <div style={{"width":"100%", "height":"100%", "position":"relative"}}>
-        <div className={"svgGroup afterIntroTransformed " + (this.state.animationCompleted ? "afterIntroTransformed" : "")}>
+        <div className={"svgGroup " + (this.state.animationCompleted ? "afterIntroTransformed" : "")}>
           <LogoAnimation completeAnimation={this.completeAnimation}/>
         </div>
-        <div className={"mondrianHolder afterIntroTransformed " + (this.state.animationCompleted ? "afterIntroTransformed" : "")}>
-          {this.rendergrid()}
+        <div className={"mondrianHolder " + (this.state.animationCompleted ? "afterIntroTransformed" : "")}>
+          {this.rendergrid(slideObject)}
         </div>
         <div className={"clearer"}/>
         <Header/>
@@ -85,55 +102,50 @@ export default IndexPage
 
 export const featuredQuery = graphql`
   query {
-          author: wordpressPost(
-            tags:
+        food:  allWordpressPost(filter:{
+            categories:
               {elemMatch: 
                 {name:
-                  {eq: "limited feature"}
+                  {eq: "sandwiches"}
                 }
-              },
+              }
+          }) {
+          edges {
+            node {
+              title
+              acf{
+                food_photo {
+                  source_url
+                }
+              }
+            }
+          }
+        }
+        allContributors: allWordpressPost(filter:{
             categories:
               {elemMatch: 
                 {name:
                   {eq: "contributor"}
                 }
               }
-            ) {
-          title
-          slug
-          acf {
-            author_name
-            author_pronoun
-            country_name
-            author_photo {
-              source_url
-            }
-          }
-    }
-     sandwich: wordpressPost(
-          tags: {
-            elemMatch:
-            {name:
-              {eq: "limited feature"}
-            }
-          },
-            categories:
-              {elemMatch: 
-                {name:
-                  {eq: "menu"}
+          } sort: { fields: [date], order: ASC }) {
+          edges {
+            node {
+              slug
+              acf{
+                country_name
+                author_name
+                sandwich_name
+                author_photo {
+                  source_url
                 }
-            
+                location_photo {
+                  source_url
+                }
+              }
+            }
           }
-        )
-    {
-      title
-      acf {
-        description
+        }
       }
-    }
-        
-  }
-
-  
 `
 
