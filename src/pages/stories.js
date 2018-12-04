9611656ee7e265855f68dbbe6c828c9c8ca380ba
Jess <jessicapeter92@gmail.com>
2018-12-04 18:46:23 +0100

    light styling on stories page

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -28,7 +28,10 @@ class Stories extends React.Component {
   render() {
     let featuredAuthor = this.state.featuredAuthor;
     let gridAuthors = [this.props.data.featured.edges[0]].concat(this.props.data.nonFeatured.edges);
-
+    let featuredSandwich = this.props.data.food.edges.find(function(node){
+      return node.node.title === featuredAuthor.title;
+    });
+    let featuredSandwichImage = featuredSandwich["node"]["acf"]["food_photo"];
       return (
         <Layout pageName={""}>
           <div className={"twelveXWrapper"}>
@@ -46,6 +49,40 @@ class Stories extends React.Component {
             </div>
             <div className={"authorGrid"}>
               <AuthorGrid authors={gridAuthors} setAuthor={this.setActiveAuthor} featuredAuthor={featuredAuthor.acf.author_name}/>
+            </div>
+          </div>
+          <div className={"storiesImageWrapper"}>
+            <div className= {"bgSquare1"}/>
+            <div className= {"bgSquare2"}/>
+            <div className = {"locationBg"} 
+                style={{"backgroundImage":"url(" + featuredAuthor.acf.location_photo.source_url + ")"}}/>
+            <div className= {"bgSquare3"}/>
+            {
+              featuredSandwichImage &&
+                <div className = {"foodBg"}
+                style={{"backgroundImage":"url(" + featuredSandwichImage.source_url + ")"}}/>
+            }
+            <div className = {"authorBox"}
+              style={{"backgroundImage":"url(" + featuredAuthor.acf.author_photo.source_url + ")"}}/>
+
+            <div className = {"inspirationBox"}>
+                <p>{featuredSandwich.node.title}</p>
+                <p>{"inspired by"}</p>
+                <p>{featuredAuthor.acf.author_name}</p>
+                <p>{"inspired by"}</p>
+                <p>{featuredAuthor.acf.country_name}</p>
+
+            </div>
+            <div className = {"excerptBox"}>
+              <p>
+                {"“" + featuredAuthor.acf.story_excerpt + "”"}
+              </p>
+            </div>
+            <div className = {"readNowBox underlineLink"}>
+              <p>{"Read " + featuredAuthor.acf.author_name + "'s"}</p>
+              <Link to={"/" + featuredAuthor.slug}>
+                <span>{featuredAuthor.title}</span>
+              </Link>
             </div>
           </div>
           <AuthorArchive 
@@ -175,6 +212,25 @@ export const contributorQuery = graphql`
                   source_url
                 },
                 location_photo {
+                  source_url
+                }
+              }
+            }
+          }
+        }
+        food:  allWordpressPost(filter:{
+            categories:
+              {elemMatch: 
+                {name:
+                  {eq: "sandwiches"}
+                }
+              }
+          }) {
+          edges {
+            node {
+              title
+              acf{
+                food_photo {
                   source_url
                 }
               }
